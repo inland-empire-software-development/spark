5672514398c983667abec5cab8b9f60eddc8110a
lloan <lloanalas@outlook.com>
2019-11-25 14:58:03 -0800

    added secret key requirement to setup action

--- models/user.js
+++ models/user.js
@@ -1,8 +1,7 @@
 module.exports = {
   user: function (db) {
-    const userTableExists = `SELECT * FROM ${process.env.DBNAME}.user`;
     const userCreateTable = `CREATE TABLE ${process.env.DBNAME}.user (id INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Unique user ID to identify a single unique user', username VARCHAR(35) NOT NULL COMMENT 'Username should be unique ', password VARCHAR(255) NOT NULL, last_login DATETIME NOT NULL, creation_date DATETIME NOT NULL DEFAULT now(), token VARCHAR(255) NOT NULL, role VARCHAR(45) NOT NULL, PRIMARY KEY (id), UNIQUE INDEX id_UNIQUE (id ASC), UNIQUE INDEX username_UNIQUE (username ASC))`;
 
-    return db.createTable(userTableExists, userCreateTable);
+    return db.createTable('user', userCreateTable);
   },
 };
