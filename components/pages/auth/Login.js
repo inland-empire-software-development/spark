e4061e00153cfb77ffd22af967516d67a8d11395
lloan <lloanalas@outlook.com>
2019-12-03 15:26:00 -0800

    remove unused message var

--- components/pages/auth/Login.js
+++ components/pages/auth/Login.js
@@ -4,8 +4,7 @@ import cookie from 'js-cookie';
 import Router from 'next/router';
 
 function Login() {
-
-  const handleLogin = e => {
+  const handleLogin = (e) => {
     e.preventDefault();
 
     const username = document.querySelector('[name="login-username"]').value;
@@ -20,8 +19,8 @@ function Login() {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({username, password}),
-    }).then(response => response.json()).then(response => {
-      const {token, message, state} = response;
+    }).then((response) => response.json()).then((response) => {
+      const {token, state} = response;
 
       if (state) {
         cookie.set('auth-token', token, {expires: 7});
@@ -31,40 +30,38 @@ function Login() {
       } else {
         // let user know failed user/pass
       }
-
     });
-
   };
 
   return (
-      <section className="auth-login">
-        <p className="uk-text-center">Sign in to your account</p>
-        <form onSubmit={e => handleLogin(e)}>
-          <div className="uk-margin">
-            <div className="uk-inline uk-width-1-1">
-              <i className="uk-form-icon fal fa-user"/>
-              <input className="uk-input uk-form-large" name="login-username" type="text" autoComplete="username"
-                     placeholder="username"/>
-            </div>
+    <section className="auth-login">
+      <p className="uk-text-center">Sign in to your account</p>
+      <form onSubmit={(e) => handleLogin(e)}>
+        <div className="uk-margin">
+          <div className="uk-inline uk-width-1-1">
+            <i className="uk-form-icon fal fa-user"/>
+            <input className="uk-input uk-form-large" name="login-username" type="text" autoComplete="username"
+              placeholder="username"/>
           </div>
-          <div className="uk-margin">
-            <div className="uk-inline uk-width-1-1">
-              <i className="uk-form-icon fal fa-lock-alt"/>
-              <input className="uk-input uk-form-large" name="login-password" type="password"
-                     autoComplete="current-password" placeholder="password"/>
-            </div>
+        </div>
+        <div className="uk-margin">
+          <div className="uk-inline uk-width-1-1">
+            <i className="uk-form-icon fal fa-lock-alt"/>
+            <input className="uk-input uk-form-large" name="login-password" type="password"
+              autoComplete="current-password" placeholder="password"/>
           </div>
-          <div className="uk-margin uk-text-right@s uk-text-center uk-text-small">
-            <a href="#" uk-switcher-item="2">Forgot Password?</a>
-          </div>
-          <div className="uk-margin">
-            <button className="uk-button bg-primary white uk-button-large uk-width-1-1">Login</button>
-          </div>
-          <div className="uk-text-small uk-text-center">
+        </div>
+        <div className="uk-margin uk-text-right@s uk-text-center uk-text-small">
+          <a href="#" uk-switcher-item="2">Forgot Password?</a>
+        </div>
+        <div className="uk-margin">
+          <button className="uk-button bg-primary white uk-button-large uk-width-1-1">Login</button>
+        </div>
+        <div className="uk-text-small uk-text-center">
             Not registered? <a href="#" uk-switcher-item="1">Create an account</a>
-          </div>
-        </form>
-      </section>
+        </div>
+      </form>
+    </section>
   );
 }
 
