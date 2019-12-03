8842d5834e81eded05a36d7cc11be23c6f698128
lloan <lloanalas@outlook.com>
2019-12-03 15:01:37 -0800

    set HttpOnly cookie for user and token

--- pages/api/login.js
+++ pages/api/login.js
@@ -3,37 +3,48 @@ import auth from '../../lib/auth';
 import client from '../../lib/redis';
 
 export default async (req, res) => {
+  // set headers
+  res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
+  res.setHeader("Content-Type", "application/javascript");
+
+  // messsages
+  const invalid = {
+    message: "invalid request",
+  };
+  const valid = {
+    message: "valid request",
+  };
+
   // Get credentials from JSON body
   const {username, password} = req.body;
 
+  // get the user - initial check
   db.getUser(escape(username)).then((user) => {
+    res.status(200);
+
+    // if user is not found, return early
     if (!user) {
-      res.send({
-        message: 'invalid user...',
-        state: false,
-      });
+      res.send(invalid);
       return false;
     }
 
-    // if user successfully logged in
+    // if user found, verify password matches
     if (auth.verifyPassword(password, user.password)) {
+      // password matched
       // create new token
       const token = auth.tokenize(username);
 
       // set token in redis
       client.setToken(username, token);
 
-      // send token back to save as a cookie in browser
-      res.send({
-        token,
-        username,
-        state: true,
-      });
+      // set HttpOnly cookie
+      res.setHeader('Set-Cookie', [`portal-token=${token}; HttpOnly`, `portal-user=${username}`]);
+      res.send(valid);
+      return true;
     } else {
-      res.send({
-        username,
-        state: false,
-      });
+      // let user know this was an invalid request
+      res.send(invalid);
+      return false;
     }
   });
 };
