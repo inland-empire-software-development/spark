d39778b7a87410dae32de6ad71dfea90970989e5
lloan <lloanalas@outlook.com>
2019-12-04 11:09:10 -0800

    tweaked formatting

--- components/hoc/Auth/Auth.js
+++
@@ -1,7 +0,0 @@
-import AuthWrapper from './AuthWrapper';
-import AuthRender from './AuthRender';
-
-// eslint-disable-next-line new-cap
-const Auth = AuthWrapper(AuthRender);
-
-export default Auth;
--- components/hoc/Auth/AuthRender.js
+++
@@ -1,5 +0,0 @@
-const AuthRender = (props) => (
-  <div>{props.children}</div>
-);
-
-export default AuthRender;
