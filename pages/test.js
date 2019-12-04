5e0d087bfc6985632ac0962f19453634185216e8
lloan <lloanalas@outlook.com>
2019-12-03 17:09:05 -0800

    removed unused code

---
+++ pages/test.js
@@ -0,0 +1,18 @@
+// import React from 'react';
+// import App from 'next/app';
+//
+// class MyApp extends App {
+//   static async getInitialProps(appContext) {
+//     // calls page's `getInitialProps` and fills `appProps.pageProps`
+//     const appProps = await App.getInitialProps(appContext);
+//
+//     return {...appProps};
+//   }
+//
+//   render() {
+//     const {Component, pageProps} = this.props;
+//     return <Component {...pageProps} />;
+//   }
+// }
+//
+// export default MyApp;
