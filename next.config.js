7f40da1476af2612043e59c9065f868456e652bb
lloan <lloanalas@outlook.com>
2019-12-05 15:10:11 -0800

    consolidated code - clean up for next.config file

--- next.config.js
+++ next.config.js
@@ -4,12 +4,10 @@ const withCSS = require("@zeit/next-css");
 const withSASS = require("@zeit/next-sass");
 const withImages = require("next-images");
 
-module.exports = {
+module.exports = withImages(withCSS(withSASS({
   webpack(config) {
     config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
 
     return config;
   },
-};
-
-module.exports = withImages(withCSS(withSASS()));
+})));
