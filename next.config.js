af36d117a9d8808f25a3a268c901d4c38535934b
lloan <lloanalas@outlook.com>
2019-12-05 16:12:16 -0800

    added support for stylelint, reformatted stylesheets, eslint chosen over prettier

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const {parsed: localEnv} = require("dotenv").config();
 const webpack = require("webpack");
+const StylelintPlugin = require('stylelint-webpack-plugin');
 const withCSS = require("@zeit/next-css");
 const withSASS = require("@zeit/next-sass");
 const withImages = require("next-images");
@@ -7,6 +8,9 @@ const withImages = require("next-images");
 module.exports = withImages(withCSS(withSASS({
   webpack(config) {
     config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
+    config.plugins.push(new StylelintPlugin({
+      fix: true,
+    }));
 
     return config;
   },
