abdc791ccaf485fb0b02c4589cffccd38bcc396b
lloan <lloanalas@outlook.com>
2019-12-10 03:08:58 -0800

    mail function initial setup functioning

---
+++ pages/api/mail.js
@@ -0,0 +1,36 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  host: process.env.MAILHOST,
+  port: process.env.MAILPORT,
+  auth: {
+    user: process.env.MAILUSER,
+    pass: process.env.MAILPASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
+
+export default async (req, res) => {
+  const mailOptions= {
+    from: '"Test Server" <test@example.com>',
+    to: 'lloanalas@outlook.com',
+    subject: "Email Test",
+    text: "This is an email test",
+  };
+
+
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err) {
+      console.log(err);
+      res.send({
+        message: "Email failed.",
+      });
+    }
+    console.log("Info: ", info);
+    res.send({
+      message: "Email successfully sent.",
+    });
+  });
+};
