ac490e49dde763d39fba831da017650501c78520
lloan <lloanalas@outlook.com>
2019-12-10 16:35:47 -0800

    css update for footer - not sticking

--- src/components/global/Footer.js
+++ src/components/global/Footer.js
@@ -2,8 +2,8 @@ function Footer(props) {
   const {stick = false} = props;
   return (
     <footer className={`uk-margin-top uk-text-center uk-padding-small ${stick ? "stick" : ""}`} >
-      <p className="uk-text-small uk-margin-remove-bottom">© 2019 authenticate.</p>
-      <p className="uk-text-small uk-margin-remove-top">An open source NextJS project</p>
+      <p className="uk-text-small uk-margin-remove-top uk-margin-remove-bottom">© 2019 authenticate.</p>
+      <p className="uk-text-small uk-margin-remove-top uk-margin-remove-bottom">An open source NextJS project</p>
     </footer>
   );
 }
