0a9e4a56d9691d6e8284884c426b98838c6946a3
lloan <lloanalas@outlook.com>
2019-12-10 19:42:46 -0800

    footer removed from auth page - no use

--- pages/auth.js
+++ pages/auth.js
@@ -13,8 +13,9 @@ function Authenticate() {
         title: `Authenticate`,
       })}
       />
-      <div id="login-container" className="uk-section uk-flex uk-flex-middle uk-animation-fade">
-        <div className="uk-width-1-1@m">
+      <section>
+        <div id="login-container" className="uk-section uk-flex uk-flex-middle uk-animation-fade">
+
           <Spinner ratio={3} classes="uk-hidden"/>
           <div className="uk-margin uk-width-large uk-margin-auto uk-card uk-card-default uk-card-body">
             <ul className="uk-tab uk-flex-center" uk-switcher="animation: uk-animation-fade">
@@ -35,8 +36,7 @@ function Authenticate() {
             </ul>
           </div>
         </div>
-      </div>
-      <Footer/>
+      </section>
     </main>
   );
 }
