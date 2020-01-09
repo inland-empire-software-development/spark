6659f715995856b80853523026d18e8996c264ea
lloan <lloanalas@outlook.com>
2020-01-09 01:37:23 -0800

    next-authenticate merge

--- src/pages.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  redirects: {
-    "/": {redirect: false},
-    "/welcome": {redirect: "/dashboard"},
-    "/logged-out": {redirect: "/dashboard"},
-    "/auth": {redirect: "/dashboard"},
-  },
-  unprotected: ["/", "/auth", "/logged-out", "/welcome"],
-};
