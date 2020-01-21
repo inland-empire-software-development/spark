5fc8133d5ee97ae05d467827116ece4c149020f3
lloan <lloanalas@outlook.com>
2020-01-20 19:43:50 -0800

    initial storybook configuration that works - addons added and tested

--- .storybook/config.js
+++
@@ -1,5 +0,0 @@
-// .storybook/config.js
-
-import { configure } from '@storybook/react';
-// automatically import all files ending in *.stories.tsx
-configure(require.context('./src/stories', true, /\.stories\.tsx?$/), module);
