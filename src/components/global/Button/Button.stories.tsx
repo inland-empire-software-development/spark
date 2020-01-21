b0e798c0b3ee9d60e306eee09015f2752aa1616b
lloan <lloanalas@outlook.com>
2020-01-21 02:12:35 -0800

    updated button example with new directory structure

--- src/components/global/Button/Button.stories.tsx
+++ src/components/global/Button/Button.stories.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+
 import "./Button.scss";
 
 import {withA11y} from '@storybook/addon-a11y';
@@ -9,29 +10,53 @@ export default {
   decorators: [withA11y],
 };
 
-export const DefaultButton = () =>
+export const DeleteButton = () =>
   <section className="uk-margin-large-top">
-    <Button label="default button"/>
+    <Button label="Remove Course"/>
   </section>;
 
-DefaultButton.story = {
+DeleteButton.story = {
   parameters: {
-    notes: 'This is the default look for the button component.',
+    notes: 'This button can be used for actions like removing a course, deleting a student, etc.',
   },
 };
 
-export const GreenButton = () =>
+export const SuccessButton = () =>
   <section className="uk-margin-large-top">
-    <Button label="green button" width="0" color="bg-green" border="border-green"/>
+    <Button label="Add Course" width="0" color="bg-green" border="border-green"/>
   </section>;
 
-export const YellowButton = () =>
+export const WarningButton = () =>
   <section className="uk-margin-large-top">
-    <Button label="yellow button" width="0" color="bg-yellow" border="border-yellow"/>
+    <Button label="Are you sure?" width="0" color="bg-yellow" border="border-yellow"/>
   </section>;
 
 
-export const BlueButton = () =>
+export const ConfirmationButton = () =>
   <section className="uk-margin-large-top">
-    <Button label="blue button" width="0" color="bg-blue" border="border-blue"/>
+    <Button label="Submit" width="0" color="bg-blue" border="border-blue"/>
   </section>;
+
+export const PrimaryButton = () =>
+  <section className="uk-margin-large-top">
+    <Button label="Save" width="0" color="bg-primary" border="border-primary"/>
+  </section>;
+
+
+export const SecondaryButton = () =>
+  <section className="uk-margin-large-top">
+    <Button label="Upload" width="0" color="bg-secondary" border="border-secondary"/>
+  </section>;
+
+
+export const TertiaryButton = () =>
+  <section className="uk-margin-large-top">
+    <Button label="Update" width="0" color="bg-tertiary" border="border-tertiary"/>
+  </section>;
+
+export const DefaultButton = () =>
+  <section className="uk-margin-large-top">
+    <Button label="Reset" width="0" text="light-gray" color="bg-snow" border="border-snow"/>
+  </section>;
+
+
