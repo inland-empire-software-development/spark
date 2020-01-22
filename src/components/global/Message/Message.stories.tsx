00b92aa01cc10c6a2f034db298f7595c2f36753d
lloan <lloanalas@outlook.com>
2020-01-22 15:57:36 -0800

    updated message component stories and signup component

--- src/components/global/Message/Message.stories.tsx
+++ src/components/global/Message/Message.stories.tsx
@@ -9,26 +9,53 @@ export default {
   decorators: [withA11y],
 };
 
-// ['dark-gray', 'light-gray', 'snow', 'blue', 'green', 'yellow', 'red', 'primary', 'secondary', 'tertiary']
 export const BasicMessage = () =>
   <section className="uk-margin-large-top">
-    <Message message="This is a basic message. Index (0)." priority={0} hidden={false}/>
+    <Message message="This is a basic message." priority={0} hidden={false}/>
   </section>;
-
-export const ReminderMessage = () =>
-  <section className="uk-margin-large-top">
-    <Message message="This is a reminder message. Index (1)." priority={1} hidden={false}/>
-  </section>;
-
 
 export const ReminderAltMessage = () =>
   <section className="uk-margin-large-top">
-    <Message message="This is an alternate reminder message. Index (2)." priority={2} hidden={false}/>
+    <Message message="This is a reminder message." priority={1} hidden={false}/>
+  </section>;
+
+
+export const ReminderMessage = () =>
+  <section className="uk-margin-large-top">
+    <Message message="This is an alternate reminder message." priority={2} hidden={false}/>
   </section>;
 
 export const ConfirmationMessage = () =>
   <section className="uk-margin-large-top">
-    <Message message="This is a confirmation message. Index (3)." priority={3} hidden={false}/>
+    <Message message="This is a confirmation message." priority={3} hidden={false}/>
   </section>;
 
+export const SuccessMessage = () =>
+  <section className="uk-margin-large-top">
+    <Message message="This is a success message." priority={4} hidden={false}/>
+  </section>;
 
+export const WarningMessage = () =>
+  <section className="uk-margin-large-top">
+    <Message message="This is a warning message." priority={5} hidden={false}/>
+  </section>;
+
+export const ErrorMessage = () =>
+  <section className="uk-margin-large-top">
+    <Message message="This is an error message." priority={6} hidden={false}/>
+  </section>;
+
+export const PrimaryMessage = () =>
+  <section className="uk-margin-large-top">
+    <Message message="This is a sample primary message." priority={7} hidden={false}/>
+  </section>;
+
+export const SecondaryMessage = () =>
+  <section className="uk-margin-large-top">
+    <Message message="This is a sample secondary message." priority={8} hidden={false}/>
+  </section>;
+
+export const TertiaryMessage = () =>
+  <section className="uk-margin-large-top">
+    <Message message="This is a sample tertiary message." priority={9} hidden={false}/>
+  </section>;
