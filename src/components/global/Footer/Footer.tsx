6bd2e07a63fda446a17a569283928d33951c8c82
lloan <lloanalas@outlook.com>
2020-01-22 20:23:32 -0800

    made footer date dynamic and added story

--- src/components/global/Footer/Footer.tsx
+++ src/components/global/Footer/Footer.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import './Footer.scss';
 
 function Footer(): JSX.Element {
+  const date = new Date();
   return (
     <footer className={`uk-margin-top uk-text-center uk-padding-small stick`} >
-      <p className="uk-text-small uk-margin-remove-top uk-margin-remove-bottom">© 2019 authenticate.</p>
-      <p className="uk-text-small uk-margin-remove-top uk-margin-remove-bottom">An open source NextJS project</p>
+      <p className="uk-text-small uk-margin-remove-top uk-margin-remove-bottom">© {date.getFullYear()} Spark.</p>
+      <p className="uk-text-small uk-margin-remove-top uk-margin-remove-bottom">An open source LMS project</p>
     </footer>
   );
 }
