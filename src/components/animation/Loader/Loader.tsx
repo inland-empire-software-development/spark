f0d7d98c68fde4245f9235c8f800465c19129dec
lloan <lloanalas@outlook.com>
2020-01-24 12:55:21 -0800

    remove ununsed code and comments

--- src/components/animation/Loader/Loader.tsx
+++ src/components/animation/Loader/Loader.tsx
@@ -8,19 +8,10 @@ export default function Loader(): JSX.Element {
   const isChrome = () => {
     if ((process as any).browser && navigator) {
       return /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
-      // if (!isChrome) {
-      //   const infinityChrome = document.querySelector('.infinityChrome') as HTMLDivElement;
-      //   const infinity = document.querySelector('.infinity') as HTMLDivElement;
-      //   console.log(infinity, infinityChrome);
-      //   if (infinityChrome && infinityChrome.hasOwnProperty('style')) infinityChrome.style.display = "none";
-      //   if (infinity && infinity.hasOwnProperty('style')) infinity.style.display = "block";
-      // }
     }
 
     return true;
   };
-
-  console.log(isChrome());
 
   return (
     <section id="infinite-loader">
