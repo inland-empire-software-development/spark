0660871e9cbfc8a75ce2939cb41c94c513cf08c9
lloan <lloanalas@outlook.com>
2020-01-27 09:42:31 -0800

    branding reverted to IESD, comment updated for Content Container component and tsconfig formatting fixed

--- .storybook/iesd.js
+++ .storybook/iesd.js
@@ -2,7 +2,7 @@ import {create} from '@storybook/theming/create';
 
 export default create({
   base: 'dark',
-  // brandTitle: 'IESD',
-  // brandUrl: 'https://iesd.com',
-  // brandImage: 'https://iesd.com/static/logos/iesd-initials-whitg.svg',
+  brandTitle: 'IESD',
+  brandUrl: 'https://iesd.com',
+  brandImage: 'https://iesd.com/static/logos/iesd-initials-white.svg',
 });
--- src/components/global/ContentContainer/ContentContainer.tsx
+++ src/components/global/ContentContainer/ContentContainer.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 function ContentContainer(props: {content: JSX.Element}): JSX.Element {
-  // only render page if user, token found in query
-
+  // fades content in
   return (
     <div className="content-container uk-section uk-flex uk-flex-middle uk-animation-fade">
       {props.content}
