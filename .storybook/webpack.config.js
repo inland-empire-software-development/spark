a50a2eee158dda7e2abccc2abef40e3bbd2f952f
lloan <lloanalas@outlook.com>
2020-01-27 08:50:29 -0800

    formatting cleanup

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,44 +2,44 @@
 
 const path = require("path");
 
-module.exports = ({ config }) => {
+module.exports = ({config}) => {
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
     loader: require.resolve('babel-loader'),
     options: {
       presets: [require.resolve('babel-preset-react-app')],
     },
-  })
-  
+  });
+
   config.module.rules.push({
     test: /\.(png|woff|woff2|eot|otf|ttf|svg)$/,
     loaders: ['file-loader'],
-    include: path.resolve(__dirname, '../')
-})
+    include: path.resolve(__dirname, '../'),
+  });
 
   config.module.rules.push({
     test: /\.s?css$/,
     use: [
-        'style-loader', 
-        'css-loader', 
-        {
-            loader: 'sass-loader',
-            options: {
-              // Prefer `dart-sass`
-              implementation: require('sass'),
-            },
+      'style-loader',
+      'css-loader',
+      {
+        loader: 'sass-loader',
+        options: {
+          // Prefer `dart-sass`
+          implementation: require('sass'),
         },
-        {
-            loader: 'sass-resources-loader',
-            options: {
-                resources: "src/style/_variables.scss", 
-            }
-        }
-      ],
+      },
+      {
+        loader: 'sass-resources-loader',
+        options: {
+          resources: "src/style/_variables.scss",
+        },
+      },
+    ],
     include: [
-        path.resolve(__dirname, "../"),
-    ]
-});
+      path.resolve(__dirname, "../"),
+    ],
+  });
 
   config.resolve.extensions.push('.ts', '.tsx');
   return config;
