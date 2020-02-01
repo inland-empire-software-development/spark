625de4e6559c814b7e9df9fea55c644734f382d1
lloan <lloanalas@outlook.com>
2020-01-31 22:29:46 -0800

    removing reference to dashboard feature

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,9 +1,7 @@
 import {useContext} from 'react';
 import Context from '../src/context';
-// import Logout from '../src/components/authenticate/LogOut/LogOut';
 import {DefaultSeo} from "next-seo";
 import SEO from "../next-seo.config";
-import AddCourse from '../src/components/dashboard/panels/AddCourse/AddCourse';
 
 function Dashboard() {
   const {user} = useContext(Context);
@@ -14,9 +12,7 @@ function Dashboard() {
         title: `${user} - dashboard`,
       })}
       />
-      {/* <h1>Hello {user}!</h1> */}
-      <AddCourse/>
-      {/* <Logout/> */}
+      <h1>Hello {user}!</h1>
     </div>
   );
 }
