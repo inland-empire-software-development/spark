be710168c91041135a7e8ccddc335e037ca4f4f9
Jacob Goodwin <jacob.goodwin@gmail.com>
2020-02-03 20:32:45 -0800

    Get dropdowns working
    Needs style updating

--- src/components/global/Sidebar/Sidebar.tsx
+++ src/components/global/Sidebar/Sidebar.tsx
@@ -24,12 +24,6 @@ const Sidebar: React.FC<SidebarProps> = (props) => {
   // state for clicked (active) and hovered (mouse hover to show submenu)
   // dashboard links
   const [activeItemLabel, setActiveItemLabel] = React.useState<string>("");
-  const [hoveredItemLabel, setHoveredItemLabel] = React.useState<string>("");
-
-  // the following state is used to tracking the absolute position of the submenu
-  const [subMenuCoordinates, setSubMenuCoordinates] = React.useState<[number, number]>([0, 0]);
-
-  // useEffect for handling navigation toggle
 
   const handleItemClicked = (item: SidebarItem) => {
     // call props onNavigate function with route path
@@ -43,15 +37,6 @@ const Sidebar: React.FC<SidebarProps> = (props) => {
     props.onNavigate(path);
   };
 
-  const handleMouseEnterItem = (event: React.MouseEvent<HTMLLIElement, MouseEvent>, itemLabel: string) => {
-    setHoveredItemLabel(itemLabel);
-    setSubMenuCoordinates([event.currentTarget.offsetTop, event.currentTarget.offsetLeft + event.currentTarget.offsetWidth]);
-  };
-
-  const handleMouseLeaveItem = () => {
-    setHoveredItemLabel("");
-  };
-
   // createMenuItems is a helper to avoid repetition
   const createMenuItems = (menuItems: SidebarItem[]): JSX.Element[] => {
     return menuItems.map((item) => {
@@ -60,69 +45,45 @@ const Sidebar: React.FC<SidebarProps> = (props) => {
       if (!item.icon || item.icon.length === 0) {
         iconStyle = "fa fa-fw icon icon-hidden";
       }
+
+      const subItems: JSX.Element | undefined = item.subItems && item.subItems.length > 0 ? (
+        <ul className="uk-nav-sub">
+          {item.subItems.map((subItem) => (
+            <li key={subItem.label}>{subItem.label}</li>
+          ))}
+        </ul>
+      ): undefined;
+
+      // create return main item with dropdown for subitems
       return (
         <li
           key={item.label}
-          className={activeItemLabel === item.label ? "active": ""}
-          onClick={() => handleItemClicked(item)}
-          onMouseEnter={(event) => handleMouseEnterItem(event, item.label)}
-          onMouseLeave={() => handleMouseLeaveItem()}>
-          <i className={iconStyle}></i>
-          {item.label}
+          className={`uk-parent ${activeItemLabel === item.label ? "active": ""}`}
+          onClick={() => handleItemClicked(item)}>
+          <span>
+            <i className={iconStyle}></i> {item.label}
+          </span>
+          {subItems}
         </li>
       );
     });
   };
 
-  // position submenu based on coordinates of currently hovered on menuItem
-  const createSubMenu = (menuItems: SidebarItem[]): JSX.Element => {
-    // choose hovered on item
-    const selected = menuItems.find((item) => item.label === hoveredItemLabel);
-    let subMenuHidden = true;
-    let subMenuItems: JSX.Element[] = [];
-    let label = "";
-    if (selected && selected.subItems && selected.subItems.length > 0) {
-      subMenuHidden = false;
-      subMenuItems = selected.subItems.map((subItem) => (
-        <li key={subItem.label} onClick={() => handleSubItemClicked(subItem.path)}>{subItem.label}</li>
-      ));
-      label = selected.label;
-    }
-
-    return (
-      <div className="sidebar-sub-menu" style={{
-        position: "absolute",
-        top: subMenuCoordinates[0],
-        left: subMenuCoordinates[1],
-        opacity: subMenuHidden ? 0 : 1,
-        visibility: subMenuHidden ? "hidden" : "visible",
-      }}
-      onMouseEnter={() => setHoveredItemLabel(label)}
-      onMouseLeave={() => handleMouseLeaveItem()}>
-        <ul>
-          {subMenuItems}
-        </ul>
-      </div>
-    );
-  };
-
   const navLinks = createMenuItems(props.menuItems);
   const accountLinks = createMenuItems(props.accountMenuItems);
-  const subMenu = createSubMenu([...props.menuItems, ...props.accountMenuItems]);
 
   return (
     <div
       className={`uk-offcanvas ${props.isOpen ? "uk-open" : ""}`}
       style={{display: "block"}}>
       <div className="uk-offcanvas-bar uk-offcanvas-bar-animation uk-offcanvas-slide sidebar-panel">
-        <ul className="uk-nav">
+        <ul className="uk-nav uk-nav-parent-icon" data-uk-nav="multiple:true;toggle:>span">
           {navLinks}
 
           <div className="section-title">Account</div>
           {accountLinks}
         </ul>
       </div>
-      {subMenu}
     </div>
   );
 };
