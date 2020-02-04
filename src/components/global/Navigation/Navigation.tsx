c8bb61a662ca350856bb436351511e38dcc426ad
Sayne Rojas <saynegrojas@yahoo.com>
2020-02-03 19:33:25 -0800

    navigation work in progress 90% complete, needs mobile link colors to be modified as NOT important.

--- src/components/global/Navigation/Navigation.tsx
+++ src/components/global/Navigation/Navigation.tsx
@@ -11,20 +11,20 @@ function Navigation(): JSX.Element {
         <div className="gradient-background">
             <nav className="uk-navbar-container uk-navbar-transparent " uk-navbar="true">
                 {/* Left portion for the logo */}
-                <div className="uk-navbar-left">
-                    <ul className="uk-navbar-nav">
-                        <li className="uk-active">
-                            <a href="#">
+                <div className="uk-navbar-left ">
+                    <ul className="uk-navbar-nav ">
+                        <li className="uk-active ">
+                            <a href="#" >
                                 <img src="/images/logo/spark-text-snow.svg" alt="spark-snow-logo" />
                             </a>
                         </li>
                         {/* Left-Middle Section for category links */}
-                        <li>
+                        <li className="uk-visible@s">
                             <Link href='#'>
-                                <a className="white home">Home</a>
+                                <a className="white home ">Home</a>
                             </Link>
                         </li>
-                        <li>
+                        <li className="uk-visible@s">
                             <a className="white">Cohorts</a>
                             <div className="uk-navbar-dropdown">
                                 <ul className="uk-nav uk-navbar-dropdown-nav">
@@ -46,7 +46,7 @@ function Navigation(): JSX.Element {
                                 </ul>
                             </div>
                         </li>
-                        <li>
+                        <li className="uk-visible@s">
                             <a className="white">Blog</a>
                             <div className="uk-navbar-dropdown">
                                 <ul className="uk-nav uk-navbar-dropdown-nav">
@@ -63,7 +63,7 @@ function Navigation(): JSX.Element {
                                 </ul>
                             </div>
                         </li>
-                        <li>
+                        <li className="uk-visible@s">
                             <a className="white">Contact</a>
                             <div className="uk-navbar-dropdown">
                                 <ul className="uk-nav uk-navbar-dropdown-nav">
@@ -82,10 +82,11 @@ function Navigation(): JSX.Element {
                         </li>
                     </ul>
                 </div>
+
                 {/* Right portion for user  */}
-                <div className="uk-navbar-right">
-                    <ul className="uk-navbar-nav">
-                        <li>
+                <div className="uk-navbar-right ">
+                    <ul className="uk-navbar-nav ">
+                        <li className="uk-visible@s">
                             {/* user mail */}
                             <Link href="/user-mail">
                                 <a className="white bg-snow">
@@ -93,7 +94,7 @@ function Navigation(): JSX.Element {
                                 </a>
                             </Link>
                         </li>
-                        <li>
+                        <li className="uk-visible@s">
                             {/* user notification */}
                             <Link href="/user-notification">
                                 <a className="white bg-snow">
@@ -101,7 +102,7 @@ function Navigation(): JSX.Element {
                                 </a>
                             </Link>
                         </li>
-                        <li>
+                        <li className="uk-visible@s">
                             {/* user profile */}
                             <Link href="/user-profile">
                                 <a className="white bg-snow profile">
@@ -133,9 +134,84 @@ function Navigation(): JSX.Element {
                                 </ul>
                             </div>
                         </li>
+                        {/* Mobile */}
+                        <li>
+                            <Link href="#offcanvas1">
+                                <a className="uk-navbar-toggle white uk-hidden@s" uk-navbar-toggle-icon="true" uk-toggle="target: #offcanvas1"></a>
+                            </Link>
+                        </li>
                     </ul>
                 </div>
             </nav>
+            <div id="offcanvas1" className="uk-offcanvas" uk-offcanvas="mode: push; flip: true">
+
+                <div className="uk-offcanvas-bar bg-white">
+                    <ul className="uk-nav uk-nav-default">
+                        <li className="uk-active">
+                            <a href="#" >
+                                {/* Change size */}
+                                <img src="/images/logo/spark-text-carbon.svg" alt="spark-snow-logo" className="offcanvas-logo" />
+                            </a>
+                        </li>
+                        <li>
+                            <Link href='#'>
+                                <a className="black home ">Home</a>
+                            </Link>
+                        </li>
+                        <li className="uk-parent">
+                            <a className="black" uk-toggle="target: #toggle-animation; mode:click" >Cohorts</a>
+                            <ul className="uk-nav-sub" id="toggle-animation" hidden >
+                                <li>
+                                    <Link href='/coharts/link-one'>
+                                        <a>Link one</a>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href='/coharts/link-two'>
+                                        <a>Link two</a>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href='/coharts/link-three'>
+                                        <a>Link three</a>
+                                    </Link>
+                                </li>
+                            </ul>
+                        </li>
+                        <li className="uk-parent">
+                            <a className="black" uk-toggle="target: #toggle-animation2; mode:click" >Blog</a>
+                            <ul className="uk-nav-sub" id="toggle-animation2" hidden>
+                                <li>
+                                    <Link href='/blog/link-one'>
+                                        <a>Link one</a>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href='/blog/link-two'>
+                                        <a>Link two</a>
+                                    </Link>
+                                </li>
+                            </ul>
+                        </li>
+                        <li className="uk-parent">
+                            <a className="black" uk-toggle="target: #toggle-animation3; mode:click">Contact</a>
+                            <ul className="uk-nav-sub" id="toggle-animation3" hidden>
+                                <li>
+                                    <Link href='/contact/link-one'>
+                                        <a>Link one</a>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href='/contact/link-two'>
+                                        <a>Link one</a>
+                                    </Link>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+
+                </div>
+            </div>
         </div>
     );
 }
