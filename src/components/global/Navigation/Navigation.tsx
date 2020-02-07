3d498f4a5d3c33ee9efe6359814ada3a41925cef
lloan <lloanalas@outlook.com>
2020-02-07 14:46:59 -0800

    add logic for navigation - wip

--- src/components/global/Navigation/Navigation.tsx
+++ src/components/global/Navigation/Navigation.tsx
@@ -1,10 +1,92 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
 import React from 'react';
 import Link from 'next/link';
 import './Navigation.scss';
 // import '../../../../public/images/logo'
 
-// Need links and scss with UIkit
-// mock user profile
+export interface Options {
+  [option: string]: any;
+}
+
+export interface ListObject {
+  icon?: string | boolean;
+  children?: Array<object> | boolean;
+  target?: string;
+  url?: string;
+  label?: string;
+};
+
+const identifyLink = (url: string) => url.indexOf(`${process.env.HOST}`);
+
+const renderLink = (obj: ListObject, opts: object) => {
+  const {url = undefined} = obj;
+  return identifyLink(url ? url : '') !== -1 ?
+    <Link href={url ? url : ''}>
+      <a {...opts}>{obj.label}</a>
+    </Link> :
+    <a {...opts} href={url}>
+      {obj.label}
+    </a>;
+};
+
+const createListItem = (
+    obj: ListObject,
+    mobile = false,
+) => {
+  const opts = {} as Options;
+
+  if (obj.icon) {
+    opts["uk-icon"] = "plus";
+  }
+
+  if (obj.target !== undefined) {
+    opts["target"] = obj.target;
+  }
+
+  if (!obj.children && mobile) {
+    opts["uk-toggle"] = "target: #offcanvas-nav";
+  }
+
+  return (
+    <li key={ obj.label? obj.label.toLowerCase() : "'"}
+      className={obj.children && mobile ?
+        "uk-parent uk-visible-toggle" : "close-canvas"}>
+
+      {renderLink(obj, opts)}
+
+      {obj.children && mobile && getMobileSubList(obj)}
+      {obj.children && !mobile && getSubList(obj)}
+    </li>
+  );
+};
+
+
+const getSubList = (obj: ListObject) =>
+  <div className="uk-navbar-dropdown">
+    <ul className="uk-nav uk-navbar-dropdown-nav">
+      {(obj.children as Array<ListObject>) ? (obj.children as Array<ListObject>).map((child: ListObject) => createListItem(child)) : ""}
+    </ul>
+  </div>;
+
+const getMobileSubList = (obj: Options) =>
+  <ul className="uk-nav-sub uk-invisible-hover">
+    {obj.children.map((child: ListObject) => createListItem(child))}
+  </ul>;
+
+const createListObject = ({
+  label,
+  url = "#",
+  icon = false,
+  children = false,
+  target,
+}: ListObject) => ({label, url, icon, children, target});
+
+[
+  {label: "Sponsors", url: "/#sponsors"},
+  {label: "Sponsors", url: "/#sponsors"},
+  {label: "Sponsors", url: "/#sponsors"},
+  {label: "Sponsors", url: "/#sponsors"},
+].map((item) => createListObject(item));
 
 function Navigation(): JSX.Element {
   return (
