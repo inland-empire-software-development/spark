54302441a2721de025f38e492d86a07f0cfa4ce5
Jacob Goodwin <jacob.goodwin@gmail.com>
2020-02-07 09:33:53 -0800

    Add close button on small screen story

--- src/components/global/Sidebar/Sidebar.stories.tsx
+++ src/components/global/Sidebar/Sidebar.stories.tsx
@@ -82,6 +82,16 @@ export const MissingIcon = () => (
   ></Sidebar>
 );
 
+export const CloseButtonOnSmallScreen = () => (
+  <Sidebar
+    menuItems={missingIconData}
+    accountMenuItems={accountItemsData}
+    isOpen={boolean("Visible?", true, "Toggles")}
+    closeButtonScreenSize="s"
+    {...actionsData}
+  ></Sidebar>
+);
+
 export const CloseButtonOnXLScreen = () => (
   <Sidebar
     menuItems={missingIconData}
