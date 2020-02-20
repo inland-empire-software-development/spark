5758ebb7c93c2202be17188cc14933f523b17880
Jacob Goodwin <jacob.goodwin@gmail.com>
2020-02-20 10:44:02 -0800

    Add missing props to Sidebar in dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -9,16 +9,38 @@ function Dashboard() {
   const {user} = useContext(Context);
   const {account, open, main} = SidebarOptions; // TODO: add ability to save data into DB and retrieve for menu generaetion
 
+  const onNavigate = (path: string) => {
+    // can pass the router here
+    console.log(`Navigating to ${path}`);
+  };
+
+  const onMenuClose = () => {
+    // call method on on context, or add useState to manage open state
+    console.log('Close menu');
+  };
+
   return (
     <main>
       <Navigation/>
-      <Sidebar accountMenuItems={account} menuItems={main} isOpen={open} />
-
+      <section>
+        <Sidebar
+          accountMenuItems={account}
+          menuItems={main}
+          isOpen={open}
+          closeButtonScreenSize={"l"}
+          onNavigate={(path) => onNavigate(path)}
+          onMenuClose={() => onMenuClose()}
+        />
+        <div className="uk-container" >
+          <section className="uk-padding">
+            <h1>Hello {user}!</h1>
+          </section>
+        </div>
+      </section>
       <DefaultSeo {...Object.assign(SEO, {
         title: `${user} - dashboard`,
       })}
       />
-      <h1>Hello {user}!</h1>
     </main>
   );
 }
