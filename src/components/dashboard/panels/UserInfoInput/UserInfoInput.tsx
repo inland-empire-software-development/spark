cf38c52a7fa155e64a5a82eb64d80d8303014afa
David <daveshuang@gmail.com>
2020-03-02 09:08:52 -0800

    initial commit

---
+++ src/components/dashboard/panels/UserInfoInput/UserInfoInput.tsx
@@ -0,0 +1,192 @@
+import React from 'react'
+import './UserInfoInput.scss'
+
+function UserInfoInput(): JSX.Element {
+  return (
+    <form className='uk-container uk-margin'>
+      <fieldset className='uk-fieldset'>
+        <legend className='uk-legend'>Personal Details</legend>
+        <hr />
+        <div className='uk-grid'>
+          <div className='uk-width-1-5'>
+            <div className='js-upload img-upload-container'>
+              <img
+                data-src='https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png'
+                src='https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png'
+                alt='Placeholder Image'
+                uk-img=''
+              />
+              <button className='uk-button uk-width-1-1 uiif-button'>
+                Browse
+              </button>
+            </div>
+          </div>
+
+          <div className='uk-width-2-5 uk-flex uk-flex-column uk-flex-between'>
+            <div>
+              <label className='uk-form-label' htmlFor='first name'>
+                First Name
+              </label>
+              <input
+                className='uk-input'
+                id=''
+                type='text'
+                placeholder='Lloan'
+              ></input>
+            </div>
+
+            <div>
+              <label className='uk-form-label' htmlFor='title'>
+                Title
+              </label>
+              <input
+                className='uk-input'
+                id=''
+                type='text'
+                placeholder='Student'
+              ></input>
+            </div>
+          </div>
+
+          <div className='uk-width-2-5 uk-flex uk-flex-column uk-flex-between'>
+            <div>
+              <label className='uk-form-label' htmlFor='last name'>
+                Last Name
+              </label>
+              <input
+                className='uk-input'
+                id=''
+                type='text'
+                placeholder='Alas'
+              ></input>
+            </div>
+
+            <div>
+              <label className='uk-form-label' htmlFor='phone number'>
+                Phone Number
+              </label>
+              <input
+                className='uk-input'
+                id=''
+                type='text'
+                placeholder='xxx-xxx-xxxx'
+              ></input>
+            </div>
+          </div>
+        </div>
+
+        <div className='uk-margin'>
+          <label className='uk-form-label' htmlFor='personal information'>
+            Personal Info
+          </label>
+          <textarea
+            className='uk-textarea'
+            id='form-h-textarea'
+            rows={5}
+            placeholder='Some text...'
+          ></textarea>
+        </div>
+      </fieldset>
+
+      <fieldset className='uk-fieldset'>
+        <legend className='uk-legend'>Change Password</legend>
+        <hr />
+
+        <div className='uk-width-1-2 uk-margin-top'>
+          <label className='uk-form-label' htmlFor='current password'>
+            Old Password
+          </label>
+          <input
+            className='uk-input'
+            id=''
+            type='password'
+            placeholder='Current Password'
+          ></input>
+        </div>
+
+        <div className='uk-width-1-2 uk-margin'>
+          <label className='uk-form-label' htmlFor='new password'>
+            New Password
+          </label>
+          <input
+            className='uk-input'
+            id=''
+            type='password'
+            placeholder='New Password'
+          ></input>
+        </div>
+
+        <div className='uk-width-1-2 uk-margin-bottom'>
+          <label className='uk-form-label' htmlFor='confirm password'>
+            Confirm Password
+          </label>
+          <input
+            className='uk-input'
+            id=''
+            type='password'
+            placeholder='Confirm Password'
+          ></input>
+        </div>
+      </fieldset>
+
+      <fieldset className='uk-fieldset'>
+        <legend className='uk-legend'>Social Links</legend>
+        <hr />
+
+        <div className='uk-grid'>
+          <div className='uk-width-1-2'>
+            <label className='uk-form-label' htmlFor='facebook'>
+              Facebook
+            </label>
+            <input
+              className='uk-input'
+              id=''
+              type='text'
+              placeholder=''
+            ></input>
+          </div>
+
+          <div className='uk-width-1-2'>
+            <label className='uk-form-label' htmlFor='twitter'>
+              Twitter
+            </label>
+            <input
+              className='uk-input'
+              id=''
+              type='text'
+              placeholder=''
+            ></input>
+          </div>
+        </div>
+
+        <div className='uk-grid'>
+          <div className='uk-width-1-2'>
+            <label className='uk-form-label' htmlFor='linkedin'>
+              LinkedIn
+            </label>
+            <input
+              className='uk-input'
+              id=''
+              type='text'
+              placeholder=''
+            ></input>
+          </div>
+
+          <div className='uk-width-1-2'>
+            <label className='uk-form-label' htmlFor='Instagram'>
+              Instagram
+            </label>
+            <input
+              className='uk-input'
+              id=''
+              type='text'
+              placeholder=''
+            ></input>
+          </div>
+        </div>
+      </fieldset>
+    </form>
+  )
+}
+
+export default UserInfoInput
