e888c57a643f210b2818b4d4d46ffd2b8caeee8b
Jacob Goodwin <jacob.goodwin@gmail.com>
2020-03-10 08:19:22 -0700

    Remove old code

--- pages/api/authenticate/login.ts
+++ pages/api/authenticate/login.ts
@@ -70,41 +70,4 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     res.end();
     return false;
   }
-
-  // db.getUser(escape(username)).then((user: any) => {
-  //   res.status(200);
-
-  //   // if user is not found, return early
-  //   if (!user) {
-  //     res.send(invalid);
-  //     res.end();
-  //     return false;
-  //   }
-
-  //   // if user found, verify password matches
-  //   if (auth.verifyPassword(password, user.password)) {
-  //     if (user.confirmation === "active") {
-  //       // password matched - create new token
-  //       const token = auth.tokenize(password);
-
-  //       // set token in redis
-  //       client.setToken(token, {user: username, userID: user.id});
-
-  //       // set HttpOnly cookies
-  //       res.setHeader('Set-Cookie', [`portal-token=${token}; path='/'; HttpOnly`, `portal-user=${username}; path='/'; HttpOnly`, `portal-user-id=${user.id}; path='/'; HttpOnly`]);
-  //       res.send(valid);
-  //       res.end();
-  //       return true;
-  //     } else {
-  //       res.send(unconfirmed);
-  //       res.end();
-  //       return false;
-  //     }
-  //   } else {
-  //     // let user know this was an invalid request
-  //     res.send(invalid);
-  //     res.end();
-  //     return false;
-  //   }
-  // });
 };
