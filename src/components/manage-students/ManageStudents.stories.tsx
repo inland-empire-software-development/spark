b8b1e10fdd55fb5d74f371bfe726c21eda7f142b
Jacob Goodwin <jacob.goodwin@gmail.com>
2020-03-17 15:05:40 -0700

    Scaffold out component and story

---
+++ src/components/manage-students/ManageStudents.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import ManageStudents from "./ManageStudents";
+
+export default {
+  title: 'ManageStudents',
+};
+
+export const DefaultManageStudents = () => (
+  <ManageStudents></ManageStudents>
+);
---
+++ src/components/manage-students/ManageStudents.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ManageStudents: React.FC = () => {
+  return <h1>Manage Students</h1>;
+};
+
+export default ManageStudents;
