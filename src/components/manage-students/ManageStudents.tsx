38a814b39632076fc721f5295abdef7fd8f44b33
Jacob Goodwin <jacob.goodwin@gmail.com>
2020-03-17 19:45:05 -0700

    Add student table styles

--- src/components/manage-students/ManageStudents.tsx
+++ src/components/manage-students/ManageStudents.tsx
@@ -11,7 +11,60 @@ const ManageStudents: React.FC = () => {
           <li><a href="#">CS101</a></li>
           <li><a href="#">GD101</a></li>
         </ul>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        // TODO - Breadcrumb with course title
+        // TODO - Select Action
+        <div className="uk-overflow-auto">
+          <table className="uk-table uk-table-divider">
+            <thead>
+              <tr>
+                <th className="uk-table-shrink">
+                  <input className="uk-checkbox" type="checkbox" />
+                </th>
+                <th className="uk-table-shrink">IMAGE</th>
+                <th className="uk-table-expand">NAME</th>
+                <th className="uk-table-expand">EMAIL</th>
+                <th className="uk-table-expand">STATUS</th>
+                <th className="uk-table-expand">ACTION</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td><input className="uk-checkbox" type="checkbox" /></td>
+                <td>
+                  <div className="student-photo">
+                    <img
+                      src="https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=b616b2c5b373a80ffc9636ba24f7a4a9"
+                      alt="User Image"
+                      data-uk-img/>
+                  </div>
+                </td>
+                <td>
+                    Karen Johnson
+                </td>
+                <td>email@aol.com</td>
+                <td>Inactive</td>
+                <td>Manage|View|Delete</td>
+              </tr>
+              <tr>
+                <td><input className="uk-checkbox" type="checkbox" /></td>
+                <td>
+                  <div className="student-photo">
+                    <img
+                      src="https://images-na.ssl-images-amazon.com/images/M/MV5BMjA3NjYzMzE1MV5BMl5BanBnXkFtZTgwNTA4NDY4OTE@._V1_UX172_CR0,0,172,256_AL_.jpg"
+                      alt="User Image"
+                      data-uk-img/>
+                  </div>
+                </td>
+                <td>
+                    Ana De Armas
+                </td>
+                <td>why@benaffleck.com</td>
+                <td>Active</td>
+                <td>Manage|View|Delete</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
