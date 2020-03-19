3b6db340ea979dc77c9ab63f11c41c2f24189b0e
Jacob Goodwin <jacob.goodwin@gmail.com>
2020-03-18 20:26:02 -0700

    A a page at home/students

---
+++ pages/students/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import DashboardLayout from "../../src/components/layouts/DashboardLayout";
+
+const Students: React.FC = () => (
+  <DashboardLayout>
+    <h1>Students page goes here once we get data</h1>
+  </DashboardLayout>
+);
+
+export default Students;
