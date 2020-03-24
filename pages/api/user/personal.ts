d5d4189ea5475952487c56453bd7e877931caa2d
David <daveshuang@gmail.com>
2020-03-24 00:59:58 -0700

    wip - updating db with form values

--- pages/api/user/personal.ts
+++ pages/api/user/personal.ts
@@ -6,8 +6,8 @@
 // =======================================================================
 
 import db from '../../../lib/db';
-import {Message} from '../../..';
-import {NextApiResponse, NextApiRequest} from 'next';
+import { Message } from '../../..';
+import { NextApiResponse, NextApiRequest } from 'next';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   // set headers
@@ -35,6 +35,47 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     userID
   } = req.body;
 
+  if (firstname) {
+    db.updateUserInfo(userID, 'first_name', firstname);
+  }
+
+  if (lastname) {
+    db.updateUserInfo(userID, 'last_name', lastname);
+  }
+
+  if (title) {
+    db.updateUserInfo(userID, 'title', title);
+  }
+  
+  if (phone) {
+    db.updateUserInfo(userID, 'phone', phone);
+  }
+
+  if (about) {
+    db.updateUserInfo(userID, 'about', about);
+  }
+
+  if (facebook) {
+    db.updateUserInfo(userID, 'facebook', facebook);
+  }
+
+  if (twitter) {
+    db.updateUserInfo(userID, 'twitter', twitter);
+  }
+
+  if (linkedin) {
+    db.updateUserInfo(userID, 'linkedin', linkedin);
+  }
+
+  if (instagram) {
+    db.updateUserInfo(userID, 'instagram', instagram);
+  }
+
+  // // Reserved for password
+  // if () {
+  //   db.updateUserInfo(userID, '', );
+  // }
+
   // check form to see how you're passing them back.
   const testMessage = {
     status: false,
@@ -44,6 +85,34 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   // example message - whatever you want to return, use this format.
   // showing return example
   res.send(testMessage);
+
+  // if (userID) {
+  //   db.updateUserInfo = function(
+  //     fname: string,
+  //     lname: string,
+  //     title: string,
+  //     phone: string,
+  //     about: string,
+  //     oldpassword: string,
+  //     password: string,
+  //     facebook: string,
+  //     twitter: string,
+  //     linkedin: string
+  //   ) {
+  //     console.log(
+  //       fname,
+  //       lname,
+  //       title,
+  //       phone,
+  //       about,
+  //       oldpassword,
+  //       password,
+  //       facebook,
+  //       twitter,
+  //       linkedin
+  //     );
+  //   };
+  // }
 
   // const {username} = req.body;
   // // turn on to see how getUser function works;
