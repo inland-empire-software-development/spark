b5401922034846438611ae2c11df2eb60b04cd1b
Jacob Goodwin <jacob.goodwin@gmail.com>
2020-04-28 12:49:31 -0700

    Responsive updates

--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -9,80 +9,81 @@ function Landing(): JSX.Element {
         <img className="uk-h8"></img>
       </div>
       {/* course info */}
-      <div className="course-info">
+      <section className="course-info">
         <div className="uk-margin-auto uk-width-3-4@m uk-card uk-card-default uk-card-body course-info-description">
           <div className="uk-grid-collapse uk-child-width-1-2@s uk-child-width-1-4@l" data-uk-grid>
-            <section className="uk-margin-remove course-info-section">
+            <div className="uk-margin-remove course-info-section">
               <h4 className="primary course-info-top">15th</h4>
               <h5 className="black course-info-mid">april</h5>
               <p className="black course-info-bottom">Start date for next cohort.</p>
-            </section>
-            <section className="uk-margin-remove course-info-section">
+            </div>
+            <div className="uk-margin-remove course-info-section">
               <h4 className="primary course-info-top">10</h4>
               <h5 className="black course-info-mid">weeks</h5>
               <p className="black course-info-bottom">Every Saturday. 12:00 PM - 3:00 PM</p>
-            </section>
-            <section className="uk-margin-remove course-info-section">
+            </div>
+            <div className="uk-margin-remove course-info-section">
               <h4 className="primary course-info-top">15</h4>
               <h5 className="black course-info-mid">seats</h5>
               <p className="black course-info-bottom">Limited number of seats</p>
-            </section>
-            <section className="uk-margin-remove bg-primary course-info-section">
+            </div>
+            <div className="uk-margin-remove bg-primary course-info-section">
               <h4 className="white course-info-top">100%</h4>
               <h5 className="black course-info-mid">scholarships</h5>
               <p className="white course-info-bottom">Apply for a full scholoarship.</p>
-            </section>
+            </div>
           </div>
         </div>
-        {/* course program */}
-        <div className="course-program uk-margin-auto uk-width-3-4@m ">
-          {/* course program description */}
-          <div className="uk-grid-large uk-grid-match course-program-description" data-uk-grid>
-            <section className="uk-align-center uk-margin-remove uk-width-1-3 course-program-section-1">
-              <h4 className="course-program-header">Course Program</h4>
-              <p className="black course-program-header-detail">Learn more about the details of our upcoming programs. </p>
-            </section>
-            <div className="uk-width-2-3">
-              <div className="uk-card uk-card-default uk-card-body">
-                <section className="uk-grid-divider uk-grid-match" data-uk-grid>
-                  <section className="uk-align-center uk-margin-remove uk-width-1-3 course-program-section-2">
-                    <h4 className="primary course-program-date-header">24th</h4>
-                    <h5 className="course-program-date">June</h5>
-                    <p className="course-program-time">Every Saturday. 1:00 PM - 3:00 PM</p>
-                  </section>
-                  <section className="uk-align-center uk-margin-remove uk-width-2-3 course-program-section-3">
-                    <h4 className="black course-program-detail">React Fundamentals:</h4>
-                    <ul className="course-program-detail-list">
-                      <li className="light-gray">
+      </section>
+
+      {/* course program */}
+      <section className="course-program uk-margin-auto uk-width-3-4@m ">
+        {/* course program description */}
+        <div className="uk-grid-medium@m course-program-description" data-uk-grid>
+          <div className="uk-width-1-4@l uk-text-center uk-text-left@m course-program-label">
+            <h4 className="course-program-header">Course Program</h4>
+            <p className="black course-program-header-detail">Learn more about the details of our upcoming programs.</p>
+          </div>
+          <div className="uk-width-3-4@l course-program-card">
+            <div className="uk-card uk-card-default uk-card-body">
+              <div className="uk-grid-divider uk-grid-match" data-uk-grid>
+                <div className="uk-align-center uk-margin-remove uk-width-1-3@m">
+                  <h4 className="primary course-program-date-header">24th</h4>
+                  <h5 className="course-program-date">June</h5>
+                  <p className="course-program-time">Every Saturday. 1:00 PM - 3:00 PM</p>
+                </div>
+                <div className="uk-align-center uk-margin-remove uk-width-2-3@m">
+                  <h4 className="black course-program-detail">React Fundamentals:</h4>
+                  <ul className="course-program-detail-list">
+                    <li className="light-gray">
                                                 Basics of React
-                      </li>
-                      <li className="light-gray">
+                    </li>
+                    <li className="light-gray">
                                                 Breakdown of the library
-                      </li>
-                      <li className="light-gray">
+                    </li>
+                    <li className="light-gray">
                                                 Creating websites with the library
-                      </li>
-                    </ul>
-                  </section>
-                </section>
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
         </div>
+      </section>
 
-      </div>
       {/* Student quote || or none?? */}
 
-      <div className="uk-card uk-card-default uk-card-body uk-flex uk-flex-column uk-flex-center student-quote-section">
+      <section className="uk-card uk-card-default uk-card-body uk-flex uk-flex-column uk-flex-center student-quote-section">
         <div className="uk-margin-large-bottom uk-margin-large-top">
           <p className="uk-text-center student-quote black uk-margin-auto">&quot;This program changed my life. Oh em gee. The instructor is amazing and the students are all passionate.&quot;</p>
           <p className="uk-text-center student black uk-margin-auto">-Cutie Pie, Student</p>
         </div>
-      </div>
+      </section>
 
 
       {/* Venue */}
-      <div className="uk-grid-collapse uk-child-width-1-2@l uk-text-center venue-section" data-uk-grid>
+      <section className="uk-grid-collapse uk-child-width-1-2@l uk-text-center venue-section" data-uk-grid>
         {/* Background image left  */}
         <div className="uk-background-contain uk-background-secondary uk-padding venue-image">
           <img src="" alt="Venue Image" />
@@ -97,37 +98,35 @@ function Landing(): JSX.Element {
           <p className="venue-county-state black">Riverside, CA 92501</p>
         </div>
 
-      </div>
+      </section>
       {/* Random quote */}
 
-      <div className="uk-card uk-card-default uk-card-body random-quote-section bg-light-gray">
+      <section className="uk-card uk-card-default uk-card-body random-quote-section bg-light-gray">
         <p className="uk-margin-auto uk-margin-large-top uk-margin-large-bottom uk-text-center white random-quote">&quot;Education is the passport to the future, for tomorrow belongs to those who prepare for it today.&quot; - Malcom X</p>
-      </div>
+      </section>
 
 
       {/* Program */}
-      <div className="uk-contianer">
-        <div className="uk-grid-collapse uk-child-width-1-2@l uk-text-center program-section" data-uk-grid>
-          <div>
-            {/* Program description left */}
-            <div className="uk-margin-auto uk-margin-auto-vertical uk-card uk-card-default uk-card-body uk-text-left uk-text-right@l program-body">
-              <h4 className="program-title">Program</h4>
-              <p className="program-description black">Spark program is a community driven learning program that provides students a working set of skills to get started building useful applications. Our courses are realistically designed and we make no false promises.</p>
-              <button className="uk-button uk-button-default uk-align-center uk-align-right@l request-button bg-primary white">Request Information</button>
-            </div>
+      <section className="uk-grid-collapse uk-child-width-1-2@l uk-text-center program-section" data-uk-grid>
+        <div>
+          {/* Program description left */}
+          <div className="uk-margin-auto uk-margin-auto-vertical uk-card uk-card-default uk-card-body uk-text-left uk-text-right@l program-body">
+            <h4 className="program-title">Program</h4>
+            <p className="program-description black">Spark program is a community driven learning program that provides students a working set of skills to get started building useful applications. Our courses are realistically designed and we make no false promises.</p>
+            <button className="uk-button uk-button-default uk-align-center uk-align-right@l request-button bg-primary white">Request Information</button>
           </div>
-          {/* Background image right  */}
-          <div className="uk-background-contain uk-background-secondary uk-flex-first uk-flex-last@l uk-padding program-image">
-            <img src="" alt="Program Image" />
-          </div>
-
         </div>
-      </div>
+        {/* Background image right  */}
+        <div className="uk-background-contain uk-background-secondary uk-flex-first uk-flex-last@l uk-padding program-image">
+          <img src="" alt="Program Image" />
+        </div>
+      </section>
+
       {/* footer */}
-      <div className="footer">
+      <section className="footer">
         <p className="copywrite">&copy; 2020 Spark.</p>
         <p className="footer-description">An open source LMS project</p>
-      </div>
+      </section>
     </div>
   );
 }
