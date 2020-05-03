8e04750577d8f7826c6d1b801b4877871d454a97
Erik Sandoval <erik.sandoval@icloud.com>
2020-05-03 16:41:07 -0700

    created basic resources html structure and added margin and padding to elements

--- pages/temp-page/index.tsx
+++ pages/temp-page/index.tsx
@@ -4,7 +4,7 @@ import DashboardLayout from "../../src/components/layouts/DashboardLayout";
 const Lessons: React.FC = () => (
   <DashboardLayout>
     <div>
-      <div className="uk-background-muted uk-flex uk-flex-between uk-flex-middle uk-width-2-3 uk-margin-medium-bottom">
+      <div className="uk-background-muted uk-flex uk-flex-between uk-flex-middle uk-width-2-3 uk-padding-small uk-margin-medium-bottom">
         <p>Dashboard</p>
         <ul className="uk-breadcrumb">
           <li>
@@ -21,17 +21,17 @@ const Lessons: React.FC = () => (
           </li>
         </ul>
       </div>
-      <div className="uk-background-default uk-flex uk-flex-between uk-flex-middle uk-width-2-3 uk-margin-medium-bottom">
+      <div className="uk-background-default uk-flex uk-flex-between uk-flex-middle uk-width-2-3 uk-padding-small uk-margin-medium-bottom">
         <div>
           <p>Project #2 - Restaurant User Interface w/ Interaction</p>
-          <p>WD105</p>
+          <p className="uk-text-meta">WD105</p>
         </div>
         <div>
           <i className="fal fa-bookmark"></i>
         </div>
       </div>
 
-      <div className="uk-background-default uk-width-2-3">
+      <div className="uk-background-default uk-width-2-3 uk-padding-small uk-margin-medium-bottom">
         <p>Overview</p>
         <p>Project Description</p>
         <p>
@@ -59,8 +59,58 @@ const Lessons: React.FC = () => (
         </div>
       </div>
 
-      <div className="uk-background-default uk-width-2-3">
+      <div className="uk-background-default uk-width-2-3 uk-padding-small">
         <p>Resources</p>
+        <form>
+          <div uk-form-custom="target: > * > span:first-child">
+            <select className="uk-width-1-1 uk-select uk-background-muted">
+              <option>All Resources</option>
+            </select>
+            <button
+              className="uk-button uk-button-default"
+              type="button"
+              tabIndex={-1}
+            >
+              <span></span>
+              <i className="fa fa-chevron-down"></i>
+            </button>
+          </div>
+        </form>
+
+        <div>
+          <div className="uk-flex uk-flex-between uk-padding-small uk-padding-remove-vertical">
+            <p>Adobe XD Docs</p>
+            <i className="fal fa-heart"></i>
+          </div>
+          <hr></hr>
+        </div>
+        <div>
+          <div className="uk-flex uk-flex-between uk-padding-small uk-padding-remove-vertical">
+            <p>Design Fundamentals</p>
+            <i className="fal fa-heart"></i>
+          </div>
+          <hr></hr>
+        </div>
+        <div>
+          <div className="uk-flex uk-flex-between uk-padding-small uk-padding-remove-vertical">
+            <p>Color Theory</p>
+            <i className="fal fa-heart"></i>
+          </div>
+          <hr></hr>
+        </div>
+        <div>
+          <div className="uk-flex uk-flex-between uk-padding-small uk-padding-remove-vertical">
+            <p>Bootstrap</p>
+            <i className="fal fa-heart"></i>
+          </div>
+          <hr></hr>
+        </div>
+        <div>
+          <div className="uk-flex uk-flex-between uk-padding-small uk-padding-remove-vertical">
+            <p>Other Resource</p>
+            <i className="fal fa-heart"></i>
+          </div>
+        </div>
       </div>
     </div>
   </DashboardLayout>
