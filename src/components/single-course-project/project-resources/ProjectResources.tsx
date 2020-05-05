dec398996e236164531f852634602d689ecf59a8
Erik Sandoval <erik.sandoval@icloud.com>
2020-05-05 00:20:37 -0700

    Styled Project Resources component

--- src/components/single-course-project/project-resources/ProjectResources.tsx
+++ src/components/single-course-project/project-resources/ProjectResources.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 
+import "./ProjectResources.scss";
+
 const ProjectResources: React.SFC = ({ children }) => {
   return (
     <div className="uk-background-default uk-width-2-3 uk-padding-small">
-      <h3>Resources</h3>
+      <h3 className="title">Resources</h3>
       <form>
         <div
           className="uk-width-1-1 uk-padding-small uk-padding-remove-horizontal uk-padding-remove-top"
@@ -13,7 +15,7 @@ const ProjectResources: React.SFC = ({ children }) => {
             <option>All Resources</option>
           </select>
           <button
-            className="uk-width-1-1 uk-flex uk-flex-between uk-flex-middle uk-button uk-button-default uk-background-muted"
+            className="dropdown-button uk-width-1-1 uk-flex uk-flex-between uk-flex-middle uk-button uk-button-default uk-background-muted uk-text-capitalize"
             type="button"
             tabIndex={-1}
           >
