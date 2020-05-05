7a023709e35493a68b196738af92a630769ebe3d
Erik Sandoval <erik.sandoval@icloud.com>
2020-05-04 23:22:01 -0700

    implemented resuable components.

--- pages/temp-page/index.tsx
+++ pages/temp-page/index.tsx
@@ -1,58 +1,23 @@
 import React from "react";
 import DashboardLayout from "../../src/components/layouts/DashboardLayout";
-import ResourceModule from "../../src/components/course-lesson/ResourceModule";
 import CourseDescription from "../../src/components/course-lesson/CourseDescription";
 import ProjectOverview from "../../src/components/course-lesson/ProjectOverview";
+import ProjectResources from "../../src/components/course-lesson/ProjectResources";
+import SubmitProject from "../../src/components/course-lesson/SubmitProject";
+import ProjectDirectory from "../../src/components/course-lesson/ProjectDirectory";
 
 const Lessons: React.FC = () => (
   <DashboardLayout>
     <div>
-      <div className="uk-background-muted uk-flex uk-flex-between uk-flex-middle uk-width-2-3 uk-padding-small uk-margin-medium-bottom">
-        <p>Dashboard</p>
-        <ul className="uk-breadcrumb">
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">Course</a>
-          </li>
-          <li>
-            <a href="#">Lectures</a>
-          </li>
-          <li>
-            <span>Project #2</span>
-          </li>
-        </ul>
-      </div>
+      <ProjectDirectory></ProjectDirectory>
 
       <CourseDescription></CourseDescription>
 
-      <ProjectOverview></ProjectOverview>
+      <ProjectOverview>
+        <SubmitProject></SubmitProject>
+      </ProjectOverview>
 
-      <div className="uk-background-default uk-width-2-3 uk-padding-small">
-        <p>Resources</p>
-        <form>
-          <div uk-form-custom="target: > * > span:first-child">
-            <select className="uk-width-1-1 uk-select uk-background-muted">
-              <option>All Resources</option>
-            </select>
-            <button
-              className="uk-button uk-button-default"
-              type="button"
-              tabIndex={-1}
-            >
-              <span></span>
-              <i className="fa fa-chevron-down"></i>
-            </button>
-          </div>
-        </form>
-
-        <ResourceModule></ResourceModule>
-        <ResourceModule></ResourceModule>
-        <ResourceModule></ResourceModule>
-        <ResourceModule></ResourceModule>
-        <ResourceModule></ResourceModule>
-      </div>
+      <ProjectResources></ProjectResources>
     </div>
   </DashboardLayout>
 );
