79c49948b6b6116dead5d97e3f5f761d8078eabc
Jacob Goodwin <jacob.goodwin@gmail.com>
2020-05-19 08:24:47 -0700

    Scaffold out dashboard home files

---
+++ src/components/dashboard/home/DashboardHome.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import DashboardHome from "./DashboardHome";
+// import {action} from "@storybook/addon-actions";
+
+export default {
+  title: 'Dashboard Home',
+};
+
+export const DefaultDashboardHome = () => (
+  <DashboardHome/>
+);
---
+++ src/components/dashboard/home/DashboardHome.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./DashboardHome.scss";
+
+const DashboardHome: React.FC<{}> = () => {
+  return (
+    <div id="dashboard-home">
+      <h1>Dashboard</h1>
+    </div>
+  );
+};
+
+
+export default DashboardHome;
