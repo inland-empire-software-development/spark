30042b7fc5057f4c5c1f786e8cdf15c52177fef2
Jacob Goodwin <jacob.goodwin@gmail.com>
2020-05-19 09:54:08 -0700

    Fill component props data

--- src/components/dashboard/home/DashboardHome.stories.tsx
+++ src/components/dashboard/home/DashboardHome.stories.tsx
@@ -1,12 +1,96 @@
 import React from "react";
 
-import DashboardHome from "./DashboardHome";
+import DashboardHome, {DashboardHomeProps} from "./DashboardHome";
 // import {action} from "@storybook/addon-actions";
 
 export default {
-  title: 'Dashboard Home',
+  title: "Dashboard Home",
 };
 
+const dashboardData: DashboardHomeProps = {
+  summaryItems: [
+    {
+      title: "Messages",
+      count: 45,
+      icon: "",
+    },
+    {
+      title: "Students",
+      count: 54,
+      icon: "",
+    },
+    {
+      title: "Courses",
+      count: 16,
+      icon: "",
+    },
+    {
+      title: "Bookmarks",
+      count: 27,
+      icon: "",
+    },
+  ],
+  notifications: [
+    {
+      id: 1,
+      title: "Message",
+      content: "Tony: Hey, have you checked out the following resources for the course? They're great!",
+    },
+    {
+      id: 2,
+      title: "Instructor",
+      content: "Assignment has been graded - Project One",
+    },
+    {
+      id: 3,
+      title: "Site Update",
+      content: "Spark application has been updated to version 1.4.5",
+    },
+    {
+      id: 4,
+      title: "Status Update",
+      content: "This is an automated server response message",
+    },
+    {
+      id: 5,
+      title: "Message",
+      content: "Felipe: Do you have any notes from the class this past Saturday? HD 4 LIFE!",
+    },
+  ],
+  currentCohort: {
+    name: "Cohort 24",
+    updates: [
+      {
+        id: 1,
+        title: "Week 1 - 01/05/2021",
+        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde esse reprehenderit debitis repellendus dolorem nostrum vel atque fuga magnam! Consequatur sapiente quod ducimus voluptatum, obcaecati optio nulla hic? Perspiciatis, nostrum.",
+      },
+      {
+        id: 2,
+        title: "Week 2 - 01/05/2021",
+        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde esse reprehenderit debitis repellendus dolorem nostrum vel atque fuga magnam! Consequatur sapiente quod ducimus voluptatum, obcaecati optio nulla hic? Perspiciatis, nostrum.",
+      },
+      {
+        id: 3,
+        title: "Week 3 - 01/05/2021",
+        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde esse reprehenderit debitis repellendus dolorem nostrum vel atque fuga magnam! Consequatur sapiente quod ducimus voluptatum, obcaecati optio nulla hic? Perspiciatis, nostrum.",
+      },
+      {
+        id: 4,
+        title: "Week 4 - 01/05/2021",
+        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde esse reprehenderit debitis repellendus dolorem nostrum vel atque fuga magnam! Consequatur sapiente quod ducimus voluptatum, obcaecati optio nulla hic? Perspiciatis, nostrum.",
+      },
+      {
+        id: 5,
+        title: "Week 5 - 01/05/2021",
+        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde esse reprehenderit debitis repellendus dolorem nostrum vel atque fuga magnam! Consequatur sapiente quod ducimus voluptatum, obcaecati optio nulla hic? Perspiciatis, nostrum.",
+      },
+    ],
+  },
+};
+
+
 export const DefaultDashboardHome = () => (
-  <DashboardHome/>
+  <DashboardHome {...dashboardData} />
 );
+
--- src/components/dashboard/home/DashboardHome.tsx
+++ src/components/dashboard/home/DashboardHome.tsx
@@ -1,10 +1,71 @@
 import React from "react";
 import "./DashboardHome.scss";
 
-const DashboardHome: React.FC<{}> = () => {
+export interface DashboardHomeProps {
+  summaryItems: SummaryItem[];
+  notifications: Notification[];
+  currentCohort: {
+    name: string;
+    updates: Update[];
+  };
+}
+
+interface SummaryItem {
+  title: string;
+  count: number;
+  icon: string; // FontAwesome icon string (pro icons available)
+}
+
+interface Notification {
+  id: number;
+  title: string;
+  content: string; // TODO: make jsx/html?
+}
+
+interface Update {
+  id: number;
+  title: string;
+  content: string; // TODO: make jsx/html?
+}
+
+const DashboardHome: React.FC<DashboardHomeProps> = (props) => {
+  const summaryItems = props.summaryItems.map((item) => (
+    <div key={item.title}>
+      <p>{item.title}</p>
+      <p>{item.count}</p>
+    </div>
+  ));
+
+  const notifications = props.notifications.map((notification) => (
+    <div key={notification.id}>
+      <p>{notification.title}</p>
+      <p>{notification.content}</p>
+    </div>
+  ));
+
+  const updates = props.currentCohort.updates.map((update) => (
+    <div key={update.id}>
+      <p>{update.title}</p>
+      <p>{update.content}</p>
+    </div>
+  ));
+
   return (
-    <div id="dashboard-home">
+    <div id="dashboard">
       <h1>Dashboard</h1>
+      <div className="uk-grid-small uk-grid-medium@s uk-child-width-1-2 uk-child-width-1-4@s uk-margin-large-top sumamry" data-uk-grid>
+        {summaryItems}
+      </div>
+      <div className="uk-grid-large uk-margin-large-top" data-uk-grid>
+        <div className="uk-width-1-3@s">
+          <h2>Notifications</h2>
+          {notifications}
+        </div>
+        <div className="uk-width-2-3@s">
+          <h2>{props.currentCohort.name}</h2>
+          {updates}
+        </div>
+      </div>
     </div>
   );
 };
