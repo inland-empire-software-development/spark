408ed743b8378d1122405b668ef7e19eef45ef2b
Jacob Goodwin <jacob.goodwin@gmail.com>
2020-05-19 14:15:06 -0700

    Fetch data using demo api

--- pages/api/course-students/demo.ts
+++ pages/api/course-students/demo.ts
@@ -1,8 +1,9 @@
 import {NextApiResponse, NextApiRequest} from 'next';
 import {Connection} from "typeorm";
-import {CourseDemo, UserDemo} from '../../../lib/entity';
+import {CourseDemo} from '../../../lib/entity';
+// import {CourseDemo, UserDemo} from '../../../lib/entity';
 import dbInit from "../../../lib/dbInit";
-import userMeta from '../../../models/userMeta';
+// import userMeta from '../../../models/userMeta';
 
 
 const getAllCoursesByInstructor = async (teacherID: string, connection: Connection) => {
@@ -11,7 +12,7 @@ const getAllCoursesByInstructor = async (teacherID: string, connection: Connecti
   const courses =
     await courseRepository.createQueryBuilder("course")
         .select(
-            ["course", "user.id", "userMeta.id", "userMeta.firstName", "userMeta.lastName", "userMeta.avatarUrl", "userMeta.status"],
+            ["course", "user.id", "user.email", "userMeta.id", "userMeta.firstName", "userMeta.lastName", "userMeta.avatarUrl", "userMeta.status"],
         )
         .leftJoin("course.users", "user")
         .leftJoin("user.userMeta", "userMeta")
@@ -36,11 +37,8 @@ const getAllCoursesByInstructor = async (teacherID: string, connection: Connecti
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const connection = await dbInit(); // established a connection if one does not yet exist
 
-  // const user = req.cookies['portal-user'];
-  // const userID = req.cookies['portal-user-id'];
-
-  const user = "jacob";
-  const userID = "2";
+  const user = req.cookies['portal-user'];
+  const userID = req.cookies['portal-user-id'];
 
   if (!user || !userID) {
     // TODO: Verifiy user's role is teacher/admin
---
+++ pages/dashboard/students/demo.tsx
@@ -0,0 +1,76 @@
+import React, {useEffect, useState} from "react";
+import {useRouter} from "next/router";
+import DashboardLayout from "../../../src/components/layouts/DashboardLayout";
+import ManageStudents, {ManageStudentsCourse} from "../../../src/components/manage-students/ManageStudents";
+
+// mapDataToCourseProps is a utility function to add types to fetched data
+// we could do this in api/user/students, but parts should be independent
+const mapDataToCourseProps = (data: any): ManageStudentsCourse[] => {
+  return data.map((course: any) => ({
+    id: course.id,
+    code: course.code,
+    name: course.name,
+    students: course.users.map((student: any) => ({
+      id: student.id,
+      email: student.email,
+      avatarUrl: student.userMeta.avatarUrl,
+      firstName: student.userMeta.firstName,
+      lastName: student.userMeta.lastName,
+      status: student.userMeta.status,
+    })),
+  }));
+};
+
+const Students = () => {
+  const [courses, setCourses] = useState<ManageStudentsCourse[]>([]);
+  const router = useRouter();
+
+  useEffect(() => {
+    fetch(process.env.HOST + "api/course-students/demo", {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'same-origin',
+    })
+        .then((res) => {
+          return res.json();
+        }) // TODO - check response code
+        .then((data) => {
+          console.log(data);
+          const courses = mapDataToCourseProps(data);
+          setCourses(courses);
+        });
+  }, []);
+
+  const handleManageUser = (id: string) => {
+    console.log(`Go to manage user page for user with id: ${id}`);
+    router.push(`/students/${id}`);
+  };
+
+  const handleViewUser = (id: string) => {
+    console.log(`Go to user page for user with id: ${id}`);
+    router.push(`/users/${id}`);
+  };
+
+  const handleRemoveStudentsFromCourse = (courseID: string, userIDs: string[]) => {
+    console.log(courseID, userIDs);
+  };
+
+  return (
+    <DashboardLayout>
+      {courses.length !== 0 && (
+        <ManageStudents
+          courses={courses}
+          onManageUser={(id) => handleManageUser(id)}
+          onViewUser={(id) => handleViewUser(id)}
+          onRemoveUsersFromCourse={({courseID, userIDs}) => handleRemoveStudentsFromCourse(courseID, userIDs)}
+        />
+      )}
+    </DashboardLayout>
+  );
+};
+
+
+export default Students;
+
